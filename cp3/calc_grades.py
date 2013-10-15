a8a716d1774dab46af32489f38aabe1adee1b9bf
David Naylor <dnaylor@cs.cmu.edu>
2013-10-14 23:31:45 -0400

    Updated grade calc script to look for special lines in .notes files

--- cp3/calc_grades.py
+++ cp3/calc_grades.py
@@ -3,20 +3,20 @@
 import os
 
 RESULTS_DIR = './results'
+NOTES_DIR = './notes'
 GRADES_DIR = './grades'
 CP_GRADES_CSV = './checkpoint-grades.csv'
+
+MESSAGE = '''Below is your raw score before any late penalty is applied.
+If you have questions about your score or would like a regrade, please email
+the course staff at staff-441@cs.cmu.edu.'''
+
 TESTS = {
 
     #
-    # HTTP 1.1  (20 pts)
-    # 
-    # 5 points from CP2 (added later)
-    # 3 points each for:
-    #  test_HEAD_headers
-    #  test_HEAD
-    #  test_GET
-    #  test_pipelining
-    #  test_browser
+    # HTTP 1.1  (15 pts)
+    #
+    # (additional 5 pts from CP2 added later)
     #
     'Headers':3,
     'HEAD':3,
@@ -26,55 +26,67 @@ TESTS = {
 
     #
     # TLS  (15 pts)
-    # 
-    # 10 pts: test_browser
-    # 5 pts: tls pipelining
     #
     'TLS Browser':10,
     'TLS pipelining':5,
 
     #
-    # CGI  (15 pts)
+    # CGI  (10 pts)
     #
-    # 5 pts: test_cgi
-    # 10 pts: test_blog
+    # 3 pts: test_cgi
+    # 7 pts: test_blog
     #
-    'CGI':5,
-    'Blog':10,
+    'CGI':3,
+    'Blog':7,
 
     #
     # ROBUSTNESS  (10 pts)
-    #
-    # 2 pts: bw
-    # 1 pt each: bad put, bad length, bad end
-    # 5 pts: test cases
     #
     'BW':2,
     'Bad PUT':1,
     'Bad Length Post':1,
     'Bad Ending GET':1,
-    'tests.txt file':5,
+    'Tests':3,
+    'Replays':2,
 
     #
     # STYLE  (5 pts)
     #
-    # 1 pt each: readme, makefile, vulnerabilities, source, replay
+    'Comments':5,
+
     #
-    'readme.txt file':1,
-    'Makefile file':1,
-    'vulnerabilities.txt file':1,
-    'Source':1,
-    'replay.[test|out] files':1
+    # CORE NETWORKING  (25 pts)
+    #
+    # (additional 5 pts from CP1 added later)
+    #
+    'Makefile':5,
+    'README':5,
+    'Modularity':5,
+    'Vulnerabilities':5,
+    'Tag':5,
+    'Other':0,
+
+    #
+    # CHECKPOINTS  (20 pts)
+    #
+    'Checkpoint 1':10,
+    'Checkpoint 2':10
 }
 
+def total_possible():
+    total = 0
+    for test in TESTS:
+        total += TESTS[test]
+    return total
+
 
 
 
 
 ##
-##  Make dictionaries for cp1 and cp2 grades. file contains lines like:
+##  Make dictionaries for cp1 and cp2 grades.
 ##
-##  andrewid,cp1score,cp2score
+##  format of each line: andrewid,cp1score,cp2score
 ##
 cp1 = {}
 cp2 = {}
@@ -83,26 +95,32 @@ with open(CP_GRADES_CSV, 'r') as cpf:
         vals = line.split(',')
         cp1[vals[0]] = int(vals[1])
         cp2[vals[0]] = int(vals[2].strip())
+cpf.closed
 
 
-for file in os.listdir(RESULTS_DIR):
-    current_file = os.path.join(RESULTS_DIR, file)
-    name, extenstion = os.path.splitext(file)
+for results_file in os.listdir(RESULTS_DIR):
+
+    # Files and paths
+    results_path = os.path.join(RESULTS_DIR, results_file)
+    name, extenstion = os.path.splitext(results_file)
     grade_file = os.path.join(GRADES_DIR, '%s.grade' % name)
+
+    # Per-student vars
     andrewid = name.split('-')[0]
+    results = {}  # testname -> points earned
+    timestamp = ''
 
     print andrewid
 
     ##
-    ##  Parse their results file
+    ##  Parse results file
     ##
-    results = {}
     current_test = ''
-    timestamp = ''
-    with open(current_file, 'r') as f:
-        for line in f:
+    with open(results_path, 'r') as resultsf:
+        for line in resultsf:
             if current_test == 'Timestamp' and 'Timestamp' in line:
                 timestamp = line.strip()
+
             if '----' in line:
                 if 'Testing' in line:
                     current_test = line.strip().split('Testing ')[1].split(' ---')[0]
@@ -110,53 +128,65 @@ for file in os.listdir(RESULTS_DIR):
                     current_test = line.strip().split(' ')[2] # for 'Inspect Source'
 
             if line.strip() == 'ok' or line.strip() == 'failed':
-                results[current_test] = (line.strip() == 'ok')
-    f.closed
+                # We don't care about tests that aren't in the TESTS dict
+                if current_test in TESTS:
+                    # all or nothing: ok => max points, failed => 0
+                    results[current_test] = TESTS[current_test] if line.strip() == 'ok' else 0
+    resultsf.closed
 
 
     ##
-    ##   Add up how many points the earned for each test
+    ##  Parse notes file
+    ##
+    ## special lines in format:  ### Testname Points-earned
+    ##
+    notes_file = os.path.join(NOTES_DIR, '%s-cp3.notes' % andrewid)
+    with open(notes_file, 'r') as notesf:
+        for line in notesf:
+            if '###' in line:
+                vals = line.split(' ')
+                results[vals[1]] = float(vals[2].strip())
+    notesf.closed
+    
+    
+    ##
+    ##  Add checkpoint scores to results dict
+    ## 
+    results['Checkpoint 1'] = cp1[andrewid]
+    results['Checkpoint 2'] = cp2[andrewid]
+
+
+    ##
+    ##   Add up how many points they earned
     ##
     score = 0
-    score_str = ''
-    late_string = '' if results['Timestamp'] else '\nLate: %s\n' % timestamp
+    details_str = ''
 
     for test in TESTS:
         possible = TESTS[test]
         earned = 0
         try:
-            earned = possible if results[test] else 0
+            earned = results[test]
         except KeyError as e:
             print '\tKey Error: %s' % e
         score += earned
-        score_str += '%s\t%d/%d\n' % (test.ljust(24), earned, possible)
+        details_str += '%s\t%g/%d\n' % (test.ljust(20), earned, possible)
     
     
-    #
-    # CORE NETWORKING  (30 pts)
-    #
-    # 5 points from CP1 (added later)
-    # 25 free points, to be manually decreased based on notes   TODO: figure this out
-    # If the score is 0 so far, don't give them the free points --- assume
-    # something was wrong with their submission
-    #  
-    core_score = 25 if score != 0 else 0
-    score += core_score
-    score_str += '%s\t%d/25\n' % ('Core Networking'.ljust(24), core_score)
-
-
+    
     ##
-    ##  Add in the checkpoint scores
-    ## 
-    score += cp1[andrewid]
-    score_str += '%s\t%d/10\n' % ('Checkpoint 1'.ljust(24), cp1[andrewid])
-    score += cp2[andrewid]
-    score_str += '%s\t%d/10\n' % ('Checkpoint 2'.ljust(24), cp2[andrewid])
+    ##  Deal with late submissions
+    ##
+    timestamp_str = 'TIMESTAMP: %s' % timestamp
+    if 'Late' in results:
+        timestamp_str += '  (%d day(s) late)' % results['Late']
+    else:
+        timestamp_str += '  (on time)'
 
 
     ##
     ##  Write final score & breakdown to a grade file
     ##
-    with open(grade_file, 'w') as outf:
-        outf.write('TOTAL SCORE: %d/105%s\n\nDetails:\n%s' % (score, late_string, score_str))
-    outf.closed
+    with open(grade_file, 'w') as gradef:
+        gradef.write('%s\n\nTOTAL SCORE: %g/%d\n%s\n\nDETAILS:\n%s' % (MESSAGE, score, total_possible(), timestamp_str, details_str))
+    gradef.closed
